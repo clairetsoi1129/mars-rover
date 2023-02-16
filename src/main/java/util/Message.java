b3d7e231467c2f1e09d5c7c783cc287be5fcada0
Claire Tsoi <clairetsoi1129@gmail.com>
2023-02-16 09:45:54 +0000

    Fix - remove additional "model" in title

--- src/main/java/util/Message.java
+++ src/main/java/util/Message.java
@@ -1,7 +1,7 @@
 package util;
 
 public class Message {
-    public static final String TITLE = "Welcome to Melody Mars model.Rover!!";
+    public static final String TITLE = "Welcome to Melody Mars Rover!!";
     public static final String WELCOME_MSG = "Welcome to Melody Mars Mission, you can have a wonderful experience " +
             "to move rovers around the surface of Mars!";
 
